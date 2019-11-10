f5fa6ff52be566eb1305df668c09aa1e349cd571
Luke Brannigan <lukebrannigan57@gmail.com>
2019-11-10 09:23:34 -0500

    merging

--- src/test/java/org/wcci/apimastery/ExperimentControllerTest.java
+++ src/test/java/org/wcci/apimastery/ExperimentControllerTest.java
@@ -1,8 +1,15 @@
 package org.wcci.apimastery;
 
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.core.Is.is;
 
 
@@ -13,6 +20,7 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -48,6 +56,14 @@ public class ExperimentControllerTest {
 		Experiment retrievedExperiment = underTest.getSingleExperiment(1L);
 		assertThat(retrievedExperiment, is(testExperiment));
 	}
+	
+//	@Test
+//	public void fetchAllIsMappedCorrectlyAndReturnsAJsonList() throws Exception {
+//		when(experimentStorage.getAllExperiments()).thenReturn(Collections.singletonList(testExperiment));
+//		mockMvc.perform(get("/api/experiments")).andDo(print()).andExpect(status().isOk())
+//				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8)).andExpect(jsonPath("$", hasSize(1)))
+//				.andExpect(jsonPath("$[0].title", is(equalTo("testexperiment"))));
+//	}
 	
 
 }
